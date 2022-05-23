eab046466daac376f5bd4f55f799d9aae20aa0d6
Victor Perez Domenech <victorrperez25@gmail.com>
2022-05-23 15:44:57 +0200

    [MOD] extraer constantes

--- src/main/java/ed/victor/Institut.java
+++ src/main/java/ed/victor/Institut.java
@@ -2,14 +2,19 @@ package ed.victor;
 
 public class Institut {
 
+	private static final int ANY = 2021;
+	private static final String CICLE_FORMATIU = "1º DAW";
+	private static final String COGNOMS_ALUMNE = "Perez";
+	private static final String NOM_ALUMNE = "Victor";
+
 	public static void main(String[] args) {
 		
 		Alumne p = new Alumne();
 		
-		p.nombre = "Victor";
-		p.cognoms = "Perez";
-		p.cicleFormatiu = "1º DAW";
-		p.any = 2021;
+		p.nombre = NOM_ALUMNE;
+		p.cognoms = COGNOMS_ALUMNE;
+		p.cicleFormatiu = CICLE_FORMATIU;
+		p.any = ANY;
 		
 		System.out.println(p.toString());
 
